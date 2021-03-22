828a73a39c5e402aa44e0997aaa7b514bab28572
Success Ologunsua <successologunsua@gmail.com>
2021-03-22 13:57:51 +0100

    cleaned up path, added dashboard.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import ApplicationForm from '../views/user/ApplicationForm.vue';
-import TakeAssessment from '../views/user/TakeAssessment.vue';
-import Questions from '../views/user/Questions.vue';
-import SuccessPage from '../views/user/SuccessPage.vue';
 
 Vue.use(VueRouter);
 
@@ -11,67 +7,72 @@ const routes = [
   {
     path: '/signup',
     name: 'SignUp',
-    component: () => import('../views/SignUp.vue'),
-  },
-  {
-    path: '/admin/create-application',
-    name: 'CreateApplication',
-    component: () => import('../views/CreateApplication.vue'),
-  },
-  {
-    path: '/admin/entries',
-    name: 'ApplicationEntries',
-    component: () => import('../views/ApplicationEntries.vue'),
-  },
-  {
-    path: '/admin/dashboard',
-    name: 'AdminDashboard',
-    component: () => import('../views/AdminDashboard.vue'),
-  },
-  {
-    path: '/application',
-    name: 'ApplicationForm',
-    component: ApplicationForm,
-  },
-  {
-    path: '/user/assessment',
-    name: 'TakeAssessment',
-    component: TakeAssessment,
-  },
-  {
-    path: '/user/success',
-    name: 'SuccessPage',
-    component: SuccessPage,
-  },
-  {
-    path: '/user/questions',
-    name: 'Questions',
-    component: Questions,
+    component: () => import('../views/user/SignUp.vue'),
   },
   {
     path: '/user/login',
     name: 'UserLogin',
-    component: () => import('../views/UserLogin.vue'),
+    component: () => import('../views/user/UserLogin.vue'),
+  },
+  {
+    path: '/application',
+    name: 'ApplicationForm',
+    component: () => import('../views/user/ApplicationForm.vue'),
+  },
+  {
+    path: '/user/dashboard',
+    name: 'Dashboard',
+    component: () => import('../views/user/Dashboard.vue'),
+  },
+  {
+    path: '/user/assessment',
+    name: 'TakeAssessment',
+    component: () => import('../views/user/TakeAssessment.vue'),
+  },
+  {
+    path: '/user/questions',
+    name: 'Questions',
+    component: () => import('../views/user/SuccessPage.vue'),
+  },
+  {
+    path: '/user/success',
+    name: 'SuccessPage',
+    component: () => import('../views/user/Questions.vue'),
   },
   {
     path: '/admin/login',
     name: 'AdminLogin',
-    component: () => import('../views/AdminLogin.vue'),
+    component: () => import('../views/admin/AdminLogin.vue'),
+  },
+  {
+    path: '/admin/dashboard',
+    name: 'AdminDashboard',
+    component: () => import('../views/admin/AdminDashboard.vue'),
+  },
+  {
+    path: '/admin/create-application',
+    name: 'CreateApplication',
+    component: () => import('../views/admin/CreateApplication.vue'),
+  },
+  {
+    path: '/admin/entries',
+    name: 'ApplicationEntries',
+    component: () => import('../views/admin/ApplicationEntries.vue'),
   },
   {
     path: '/admin/assessment',
     name: 'ComposeAssessment',
-    component: () => import('../views/ComposeAssessment.vue'),
+    component: () => import('../views/admin/ComposeAssessment.vue'),
   },
   {
     path: '/admin/history',
     name: 'AssessmentHistory',
-    component: () => import('../views/AssessmentHistory.vue'),
+    component: () => import('../views/admin/AssessmentHistory.vue'),
   },
   {
     path: '/admin/results/:batchId',
     name: 'AssessmentResults',
-    component: () => import('../views/AssessmentResults.vue'),
+    component: () => import('../views/admin/AssessmentResults.vue'),
   },
 ];
 
