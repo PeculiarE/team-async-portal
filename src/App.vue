e83d5e99ac14f55489fc5e4722b64c0ce7599be0
perhisohwode@gmail.com <PeculiarE>
2021-03-22 13:22:14 +0100

    edited for merging

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-    </div>
     <router-view/>
   </div>
 </template>
@@ -9,15 +7,30 @@
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
 
+:root {
+--enyata-purple: #7557D3;
+--text-primary: #2B3C4E;
+--text-secondary-small: #4F4F4F;
+}
+
 #app {
   font-family: 'Lato', sans-serif;
-  max-width: 1440px;
-  max-height: 1024px;
+  color: #2b3c4e;
+  background-color: #FDFDFF;
 }
+
 #admin-sidebar-top-section {
   height: 33%;
   padding-top: 15%;
   background: #5ABEFD;
+  text-align: center;
+}
+#sidebar-top-section {
+  height: 25%;
+  width: 100%;
+  background: #2B3C4E;
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.25);
+  border: 1px solid #000000;
   text-align: center;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,53 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import ApplicationForm from '../views/user/ApplicationForm.vue';
+import TakeAssessment from '../views/user/TakeAssessment.vue';
+import Questions from '../views/user/Questions.vue';
+import SuccessPage from '../views/user/SuccessPage.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: () => import('../views/SignUp.vue'),
+  },
+  {
+    path: '/admin/create-application',
+    name: 'CreateApplication',
+    component: () => import('../views/CreateApplication.vue'),
+  },
+  {
+    path: '/admin/entries',
+    name: 'ApplicationEntries',
+    component: () => import('../views/ApplicationEntries.vue'),
+  },
+  {
+    path: '/admin/dashboard',
+    name: 'AdminDashboard',
+    component: () => import('../views/AdminDashboard.vue'),
+  },
+  {
+    path: '/application',
+    name: 'ApplicationForm',
+    component: ApplicationForm,
+  },
+  {
+    path: '/user/assessment',
+    name: 'TakeAssessment',
+    component: TakeAssessment,
+  },
+  {
+    path: '/user/success',
+    name: 'SuccessPage',
+    component: SuccessPage,
+  },
+  {
+    path: '/user/questions',
+    name: 'Questions',
+    component: Questions,
+  },
   {
     path: '/user/login',
     name: 'UserLogin',
