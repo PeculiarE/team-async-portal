c4d9a0eea023d231cc8b8ea62f499980bc0b1ffe
Success Ologunsua <successologunsua@gmail.com>
2021-04-12 15:00:13 +0100

    confirm new password

--- src/components/PasswordReset.vue
+++ src/components/PasswordReset.vue
@@ -5,17 +5,60 @@
     <div >
       <b-form @submit.prevent = "resetPassword" method= 'put'>
         <b-form-group
-        id="input-group-2"
+        id="input-group-1"
         class="text-left label"
         >
+        <div class="password-div">
           <b-form-input
-            id="input-2"
-            v-model="password"
+            id="input-1"
             placeholder="Enter new password"
-            type="password"
             data-toggle="password"
             required
+              @keyup="validatePassword()"
+              :type="showPassword ? 'text' : 'password'"
+              v-model="userDetails.password"
           ></b-form-input>
+          <span @click="togglePassword" v-show='showPassword'>
+              <i class="fas fa-eye-slash"></i>
+            </span>
+            <span @click="togglePassword" v-show="!showPassword">
+              <i class="fas fa-eye"></i>
+            </span>
+        </div>
+        <b-form-invalid-feedback :state="feedbackPassword">
+              Password is weak.
+            </b-form-invalid-feedback>
+            <b-form-valid-feedback :state="feedbackPassword">
+              Password is strong!
+          </b-form-valid-feedback>
+        </b-form-group>
+
+        <b-form-group
+        id="input-group-2"
+        class="text-left label">
+          <div class="password-div">
+          <b-form-input
+            id="input-2"
+            placeholder="Confirm new password"
+            data-toggle="password"
+            required
+            :type="showConfirmPassword ? 'text' : 'password'"
+              v-model="userDetails.confirmPassword"
+              @keyup="checkPasswords()"
+          ></b-form-input>
+          <span @click="toggleConfirmPassword" v-show='showConfirmPassword'>
+              <i class="fas fa-eye-slash"></i>
+            </span>
+            <span @click="toggleConfirmPassword" v-show="!showConfirmPassword">
+              <i class="fas fa-eye"></i>
+            </span>
+        </div>
+            <b-form-invalid-feedback :state="feedbackConfirmPassword">
+            Passwords do not match
+          </b-form-invalid-feedback>
+          <b-form-valid-feedback :state="feedbackConfirmPassword">
+            Passwords match!
+          </b-form-valid-feedback>
           <br>
         </b-form-group>
         <div >
@@ -34,7 +77,14 @@ export default {
   name: 'PasswordReset',
   data() {
     return {
-      password: '',
+      userDetails: {
+        password: '',
+        confirmPassword: '',
+      },
+      showPassword: false,
+      showConfirmPassword: false,
+      feedbackPassword: null,
+      feedbackConfirmPassword: null,
     };
   },
   methods: {
@@ -48,6 +98,31 @@ export default {
       };
       this.newPassword(obj);
       this.$router.push('/changesuccess');
+    },
+    validatePassword() {
+      const strongPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$^!%#*?&.,/+-_])[A-Za-z\d@$^!%#*?&.,/+-_]{8,}$/;
+      if (this.userDetails.password.match(strongPassword)) {
+        this.feedbackPassword = true;
+        this.checkPasswords();
+        return this.feedbackPassword;
+      }
+      this.feedbackPassword = false;
+      this.checkPasswords();
+      return this.feedbackPassword;
+    },
+    checkPasswords() {
+      if (this.userDetails.password === this.userDetails.confirmPassword) {
+        this.feedbackConfirmPassword = true;
+        return this.feedbackConfirmPassword;
+      }
+      this.feedbackConfirmPassword = false;
+      return this.feedbackConfirmPassword;
+    },
+    togglePassword() {
+      this.showPassword = !this.showPassword;
+    },
+    toggleConfirmPassword() {
+      this.showConfirmPassword = !this.showConfirmPassword;
     },
   },
 };
@@ -71,8 +146,16 @@ form {
 }
 input {
   width: 400px;
-  /* margin-top: 10px; */
   height: 50px;
+}
+input-1, input-2{
+    width: 379px;
+    height: 41px;
+    border: 1.5px solid #BDBDBD;
+    outline: none;
+    border-radius: 4px;
+    margin-bottom: 19px;
+    width: 379px;
 }
 button {
   width: 200px;
@@ -84,4 +167,18 @@ button {
   border: 1px solid #7557d3;
   border-radius: 4px;
 }
+.fa-eye, .fa-eye-slash {
+    font-size: 13px;
+    margin-left: -30px;
+    color: #b8b8b9;
+}
+.password-div{
+    display: flex;
+    /* flex-flow: column nowrap; */
+    align-items: center;
+  }
+  .password-div span {
+    cursor: pointer;
+    margin-top: -15px;
+  }
 </style>
